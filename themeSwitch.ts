568a989974f6aa0c02035237ce68f71a0066d176
KauanAzevedo <kauan.azevedo@fontespromotora.com.br>
2022-11-17 07:59:25 -0300

    feat: javascript

--- themeSwitch.ts
+++ themeSwitch.ts
@@ -1,23 +1,23 @@
-const toggleSwitch = <HTMLInputElement> document.querySelector('#theme-switch');
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
+// const toggleSwitch = <HTMLInputElement> document.querySelector('#theme-switch');
+// const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
 
 
-if(currentTheme){
-    document.documentElement.setAttribute('data-theme', currentTheme);
+// if(currentTheme){
+//     document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if(currentTheme === 'dark'){
-        toggleSwitch.checked = true;
-    }
-}
+//     if(currentTheme === 'dark'){
+//         toggleSwitch.checked = true;
+//     }
+// }
 
-// e: {target: HTMLButtonElement}
-function switchTheme(e: {target: HTMLInputElement}){
-    if(e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-    }
-}
-toggleSwitch.addEventListener('click', () => switchTheme);
+// // e: {target: HTMLButtonElement}
+// function switchTheme(e: {target: HTMLInputElement}){
+//     if(e.target.checked) {
+//         document.documentElement.setAttribute('data-theme', 'dark');
+//         localStorage.setItem('theme', 'dark');
+//     } else {
+//         document.documentElement.setAttribute('data-theme', 'light');
+//         localStorage.setItem('theme', 'light');
+//     }
+// }
+// toggleSwitch.addEventListener('click', () => switchTheme);
